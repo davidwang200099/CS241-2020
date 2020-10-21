7aac883d670c3881992b60ed15a78b2614754465
davidwang200099 <davidwang200099@163.com>
2020-10-21 21:22:26 +0800

    add hw5

---
+++ hw5/Tree.cpp
@@ -0,0 +1,47 @@
+#include "Tree.h"
+
+
+// ==================== k-d tree ======================
+kdTree::kdTree(){
+    
+}
+
+void kdTree::insert(kdNode* root, int arr[], int dim){
+
+}
+
+kdNode* kdTree::search(kdNode* root, int arr[], int dim){
+
+}
+
+void kdTree::remove(kdNode* root, int arr[], int dim){
+
+}
+
+kdNode* kdTree::findMin(kdNode* root, int dimCmp, int dim){
+
+}
+
+vector<struct kdNode*> kdTree::rangeSearch(kdNode* root, int dim, int lower_bound[], int upper_bound[]){
+
+}
+
+
+// ==================== trie (prefix tree) ======================
+Trie::Trie() {
+    
+}
+
+void Trie::insert(string word) {
+    
+}
+
+
+bool Trie::search(string word) {
+    
+}
+
+
+bool Trie::startsWith(string prefix) {
+    
+}
